b929b9ac539bcbb4bb2d8bb68482e6b34a5b1819
Michael Egbinola <michaelmarketing25@gmail.com>
2022-11-12 08:14:35 -0800

    Completed sets

--- 05-DATA-STRUCTURES_MODERN-OPERATORS_STRINGS/11sets.js
+++ 05-DATA-STRUCTURES_MODERN-OPERATORS_STRINGS/11sets.js
@@ -1,1 +1,26 @@
+"use strict";
 
+//sets are alike arrays, but they return unique data. Used to remove duplicate values of an array
+const orderSet = new Set([
+  "Pasta",
+  "Pizza",
+  "Pizza",
+  "Risotto",
+  "Pasta",
+  "Pizza",
+]);
+
+console.log(orderSet);
+console.log(new Set("Michaeli"));
+
+console.log(orderSet.size); //we use size to check the size of the set
+console.log(orderSet.has("Pizza")); //check if an element is available in the set
+orderSet.add("Garlic Bread"); //add an element to the set
+orderSet.delete("Risotto"); // delete an element in the set
+console.log(orderSet);
+
+for (const order of orderSet) console.log(order); //loop through a set
+
+const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
+const staffUnique = [...new Set(staff)]; //converted the set to an array using the spread operator
+console.log(staffUnique);
