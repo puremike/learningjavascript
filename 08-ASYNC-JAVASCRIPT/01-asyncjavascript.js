7745c1bf2da4fa73441fb769e32804f7ed3f4044
Michael Egbinola <michaelmarketing25@gmail.com>
2023-02-28 00:54:08 -0800

    Started 01-asyncjavascript

---
+++ 08-ASYNC-JAVASCRIPT/01-asyncjavascript.js
@@ -0,0 +1,4 @@
+//Asynchronous code is executed after a task that runs in the "background" finishes
+//Asynchronous code is non-blocking
+//Execution doesn't wait for an asynchronous task to finish it work
+//Example of an asynchronous code is a "Timer with callback"
