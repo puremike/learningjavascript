b8c9a79f2342f552a1123ae3fc09a57de9d69743
Michael Egbinola <michaelmarketing25@gmail.com>
2023-03-01 01:22:12 -0800

    Completed AJAX request type

--- 08-ASYNC-JAVASCRIPT/01-asyncjavascript.js
+++ 08-ASYNC-JAVASCRIPT/01-asyncjavascript.js
@@ -6,3 +6,14 @@
 //AJAX - Stands for Asynchronous JavaScript and XML: allows us to communicate with remote web servers in an asynchronous way. With AJAX calls, we can request data from web servers dynamically.
 
 //API - API stands for Application Programming Interface, piece of software that can be used by another piece of software, in order to allow applications to talk to each other. Examples of APIs - DOM API, Geolocation API, Own Class API, Online API
+
+//A simple XMLHttpRequest for REST COUNTRIES API
+
+const xml = new XMLHttpRequest();
+xml.open("GET", "https://restcountries.com/v3.1/name/nigeria");
+xml.send();
+
+xml.addEventListener("load", function () {
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+});
---
+++ 08-ASYNC-JAVASCRIPT/02-starter/script.js
@@ -0,0 +1,8 @@
+// const firstTextAreaElement = document.querySelector(".text-element");
+// const secondTextAreaElement = document.querySelector('.second-text-element')
+// const btnElement = document.querySelector(".btn");
+
+// btnElement.addEventListener("click", function () {
+//   const textAreaInput = textAreaElement.input;
+//   const translateFirstTextAreaElement = ``
+// });
