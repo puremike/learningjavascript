6d6a23370ad89048fb302ff247235f4a6567b20c
Michael Egbinola <michaelmarketing25@gmail.com>
2023-03-03 11:21:00 -0800

    Completed chaining promises

--- 08-ASYNC-JAVASCRIPT/01-starter/script.js
+++ 08-ASYNC-JAVASCRIPT/01-starter/script.js
@@ -6,15 +6,32 @@ const countriesContainer = document.querySelector('.countries');
 const renderCountry = function (data, className = '') {
   const html = `
   <article class="country ${className}">
-   <img class="country__img" src="${data.flags.png}" />
+   <img class="country__img" src="${data.flags?.png}" />
     <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
+      <h3 class="country__name">${data.name?.common || data.name}</h3>
       <h4 class="country__region">${data.region}</h4>
       <p class="country__row"><span>👫</span>${(
         +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
-      <p class="country__row"><span>💰</span>${data.currencies.NGN.name}</p>
+      ).toFixed(1)} M people</p>
+      <p class="country__row"><span>🗣️</span>${
+        data.languages?.eng ||
+        data.languages?.por ||
+        data.languages?.spa ||
+        data.languages?.fra ||
+        data.languages?.jpn ||
+        data.languages?.ara ||
+        data.languages?.[0]?.name
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        data.currencies?.NGN?.name ||
+        data.currencies?.CAD?.name ||
+        data.currencies?.USD?.name ||
+        data.currencies?.EUR?.name ||
+        data.currencies?.JPY?.name ||
+        data.currencies?.AED?.name ||
+        data.currencies?.GBP?.name ||
+        data.currencies?.[0]?.name
+      }</p>
     </div>
   </article>
   `;
@@ -62,12 +79,29 @@ const renderCountry = function (data, className = '') {
 //CONSUME A PROMISE
 
 const getCountryData = function (country) {
+  //country 1
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
     .then(data => {
       console.log(data);
       renderCountry(data[0]);
+
+      //country 2
+      const neighbour = data[0].borders?.[0]; //optional chaining
+      console.log(neighbour);
+
+      if (!neighbour) return;
+
+      //fetching data for country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      renderCountry(data, 'neighbour');
     });
 };
 
 getCountryData('nigeria');
+
+//CHAINING PROMISES
