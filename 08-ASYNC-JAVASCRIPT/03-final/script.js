c7804998f62b7668a2d15be0736c941e21072488
Michael Egbinola <michaelmarketing25@gmail.com>
2023-03-13 23:45:48 -0700

    Consuming Promises with Async Await, ES7

--- 08-ASYNC-JAVASCRIPT/03-final/script.js
+++ 08-ASYNC-JAVASCRIPT/03-final/script.js
@@ -8,30 +8,16 @@ const wait = function (second) {
 
 const h2Element = document.querySelector("h2");
 
-// function createImage(imgPath) {
-//   return new Promise(function (resolve, reject) {
-//     const img = document.createElement("img");
-//     img.src = imgPath;
-//     h2Element.after(img);
-
-//     resolve(img);
-//   });
-// }
-
-// with event handler
-
 function createImage(imgPath) {
   return new Promise(function (resolve, reject) {
     const img = document.createElement("img");
     img.src = imgPath;
+    h2Element.after(img);
 
     img.addEventListener("load", function () {
-      h2Element.after(img);
-      resolve(img);
-    });
-
-    img.addEventListener("load", function () {
-      reject(new Error("Images not found!!"));
+      if (resolve(img)) {
+        h2.after(img);
+      } else reject(new Error("Images not found!!"));
     });
   });
 }
@@ -44,7 +30,7 @@ createImage("./img/pexels-photo-544268.jpeg")
   })
   .then(() => {
     currentImg.style.display = "none";
-    return createImage("./img/pexels-phto-5615130.jpeg");
+    return createImage("./img/pexels-photo-5615130.jpeg");
   })
   .then((img) => {
     console.log(`Image 2 Loaded Successfully!`);
